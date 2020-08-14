ffa8f434897000fbf6e9a64212a4eec80b4442dd
Luyu Zeng <zengluyu252@gmail.com>
2020-08-14 14:47:33 -0400

    before pull

--- public/js/index.js
+++ public/js/index.js
@@ -3,8 +3,11 @@ $(document).ready(() => {
     // if no user login and signup buttons display=block, logout btn display=none
     // if there is a user login and signup buttons display=none and logout btn display=block
     // when user tap on a specific box, it will bring the user to the specific retuarant page
-    if (!userData.email || !userData.password) {
-        return $("a#logoutlink").style.display = "none";
-    }
+    $.get("/api/user_data").then(data => {
+        if (!data.email || !data.password) {
+            return $("a#logoutlink").style.display = "none";
+        }
+    });
+    
 
 });
