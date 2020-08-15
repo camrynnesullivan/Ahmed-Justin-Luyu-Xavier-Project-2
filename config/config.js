7f02d8225fde7ee1afba9ef7a74e265e09423cd2
Luyu Zeng <zengluyu252@gmail.com>
2020-08-15 13:15:32 -0400

    before reslove

--- config/config.js
+++ config/config.js
@@ -2,7 +2,11 @@ require("dotenv");
 module.exports = {
   development: {
     username: "root",
+<<<<<<< HEAD
     password: process.env.DB_Pass,
+=======
+    password: DB_PASS,
+>>>>>>> b358743409fcf474efff25f3b359d2836d3968a7
     database: "vegan_db",
     host: "127.0.0.1",
     dialect: "mysql",
