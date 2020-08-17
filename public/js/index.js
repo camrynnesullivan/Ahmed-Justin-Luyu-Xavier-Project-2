95a50e7df5633827255b9e7b031644d08724bb1d
Luyu Zeng <zengluyu252@gmail.com>
2020-08-17 10:19:07 -0400

    Updated backBtn and LogoutBtn

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,15 @@ $(document).ready(() => {
   // if no user login and signup buttons display=block, logout btn display=none
   // if there is a user login and signup buttons display=none and logout btn display=block
   // when user tap on a specific box, it will bring the user to the specific retuarant page
+  
   $.get("/api/user_data").then((data) => {
     if (!data.email || !data.password) {
       return ($("a#logoutlink").style.display = "none");
     }
   });
   $("#logoutBtn").on("click", () => {
-    $.get("/logout");
+    // $.get("/logout");
+    location.href = "/logout";
   });
   $(".checkout").on("click", (e) => {
     let key = e.target.getAttribute("data");
